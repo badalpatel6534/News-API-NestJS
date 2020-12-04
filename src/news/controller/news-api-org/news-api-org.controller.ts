dff739048e684f0969497171923402b253d9ddb0
badalpatel <badalpatel6534@gmail.com>
2020-12-05 02:22:41 +0530

    change Param to Query decorator

--- src/news/controller/news-api-org/news-api-org.controller.ts
+++ src/news/controller/news-api-org/news-api-org.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
 import { NewsApiOrgService } from 'src/news/service/news-api-org/news-api-org.service';
-
 
 @Controller('news-api-org')
 export class NewsApiOrgController {
     constructor(private newsApiOrgService: NewsApiOrgService) {}
 
-    @Get('topHeadlinesNews/:category/:country')
-        async getTopHeadlinesNews(@Param("category") category: string,@Param("country") country: string) {
-        console.log('category', category);
-        console.log('country', country);
+    @Get('topHeadlinesNews')
+        async getTopHeadlinesNews(@Query("category") category: string,@Query("country") country: string) {
         return await this.newsApiOrgService.getTopHeadlinesNews(category,  country);
     }
 }
